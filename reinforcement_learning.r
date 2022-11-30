744ebe3a0849570e4d9075e65a819b7330b690d4
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-11-30 11:17:37 +0000

    Update

--- reinforcement_learning.r
+++ reinforcement_learning.r
@@ -16,7 +16,11 @@ env = gridworldEnvironment
 data = sampleExperience(N=1000, env=env, states=states, actions=actions)
 data
 
-tictactoe
 model = ReinforcementLearning(data = tictactoe, s = "State", a = "Action", r = "Reward", s_new = "NextState", iter = 1)
 model
+
+control = list(alpha = 0.1, gamma = 0.1, epsilon = 0)
+
+model2 = ReinforcementLearning(data = tictactoe,  s = "State", a = "Action", r = "Reward", s_new = "NextState", iter = 10, control = control)
+model2
 
